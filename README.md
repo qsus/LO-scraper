1baeab30508ead3300ada07eb44120cc97ff755d
qsus <106612195+qsus@users.noreply.github.com>
2024-09-30 12:51:03 +0000

    create git repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# LO-scraper
+Scrape number of contestants registered for [Logická olympiáda](https://www.logickaolympiada.cz/) (Czech logic olympiad).
+## TODO:
+- replace UTC time with local time (CEST)
---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import csv
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def getRegistered():
+	url = 'https://www.logickaolympiada.cz/'
+	response = requests.get(url)
+
+	if response.status_code != 200:
+		logError(f"status code {response.status_code}")
+		return False
+
+
+	# find the correct tag
+	soup = BeautifulSoup(response.text, 'html.parser')
+	span_tag = soup.find('span', string="Počet registrovaných soutěžících:   ")
+	parent_tag = span_tag.parent
+	sibling_tag = parent_tag.find_next_sibling('strong')
+
+	number = sibling_tag.get_text(strip=True).split()[0]
+	return number
+
+def logCsv(columns):
+    with open("data.csv", mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)
+
+
+while True:
+	# wait for the next hour
+	now = datetime.now()
+	sleeptime = (60 - now.minute) * 60 - now.second
+	print(f"Sleeping {sleeptime} seconds.")
+	time.sleep(sleeptime)
+
+	now = datetime.now()
+	
+	# scrape
+	registered = getRegistered()
+
+	# print and log
+	print(f"It is {now.strftime('%Y-%m-%d %H:%M')} and {registered} people are registered.")
+	logCsv([
+		now.strftime("%Y-%m-%d %H:%M"),
+		registered
+	])
